6fd2a8676e947bf5c0295c7d489e5723430afbad
bluepnwage <a.carty2555@gmail.com>
2023-05-11 20:35:22 -0400

    [ui] - add custom colors to dropdown item and ring to dropdown content

--- packages/ui/src/dropdown/index.tsx
+++ packages/ui/src/dropdown/index.tsx
@@ -1,25 +1,44 @@
 import * as RadixDropdown from "@radix-ui/react-dropdown-menu";
+import { cva } from "cva";
+import type { VariantProps } from "cva";
 
-function Item({
-  icon,
-  rightSection,
-  ...props
-}: RadixDropdown.DropdownMenuItemProps & { icon?: React.ReactNode; rightSection?: React.ReactNode }) {
+const itemStyles = cva(
+  `ui-group ui-text-sm ui-leading-none ui-rounded-[3px] ui-flex ui-items-center ui-h-8 ui-px-[5px]
+   ui-relative ui-pl-[25px] ui-select-none ui-outline-none`,
+  {
+    variants: {
+      color: {
+        warn: `ui-text-warn-600 dark:ui-text-warn-200 data-[highlighted]:ui-bg-warn-200/30
+        data-[highlighted]:dark:ui-bg-warn-600/30`,
+        error: `ui-text-error-600 dark:ui-text-error-200 
+        data-[highlighted]:ui-bg-error-200/30 data-[highlighted]:dark:ui-bg-error-600/30`,
+        default: `dark:ui-text-neutral-100 ui-text-neutral-900  
+      data-[disabled]:ui-text-neutral-200 data-[highlighted]:ui-bg-primary-200/30 
+      data-[highlighted]:ui-text-primary-600 data-[highlighted]:dark:ui-text-primary-200
+      data-[disabled]:dark:ui-text-neutral-500 data-[highlighted]:dark:ui-bg-primary-600/30
+       dark:ui-text-neutral-100`
+      }
+    },
+    defaultVariants: {
+      color: "default"
+    }
+  }
+);
+
+type ItemProps = VariantProps<typeof itemStyles> & {
+  icon?: React.ReactNode;
+  rightSection?: React.ReactNode;
+} & RadixDropdown.MenuItemProps;
+
+function Item({ icon, rightSection, color, className, ...props }: ItemProps) {
   return (
-    <RadixDropdown.Item
-      {...props}
-      className={`ui-group ui-text-sm ui-leading-none  ui-rounded-[3px] ui-flex ui-items-center
-  ui-h-8 ui-px-[5px] ui-relative ui-pl-[25px] ui-select-none ui-outline-none 
-  dark:ui-text-neutral-100 ui-text-neutral-900  
-  data-[disabled]:ui-text-neutral-200 data-[highlighted]:ui-bg-primary-600 
-  data-[highlighted]:ui-text-primary-50 data-[disabled]:dark:ui-text-neutral-500 
-  data-[highlighted]:dark:ui-bg-primary-500 dark:ui-text-neutral-100`}
-    >
+    <RadixDropdown.Item {...props} className={itemStyles({ className, color })}>
       {icon ? <span className="mr-2 inline-block">{icon}</span> : null}
       {props.children}
       {rightSection ? (
         <div
-          className={`ml-auto pl-[20px] ui-text-neutral-400 group-data-[highlighted]:ui-text-primary-50 
+          className={`ui-ml-auto ui-pl-[20px] ui-text-neutral-400 group-data-[highlighted]:ui-text-primary-300 
+          group-data-[highlighted]:dark:ui-text-primary-400
             group-data-[disabled]:dark:ui-text-neutral-600 dark:ui-text-neutral-500 
             group-data-[disabled]:ui-text-neutral-200`}
         >
@@ -41,10 +60,9 @@ function Content({ arrow, portalProps, ...props }: ContentProps) {
       <RadixDropdown.Content
         {...props}
         sideOffset={5}
-        className={`ui-min-w-[220px] ui-ring-1 dark:ui-ring-0 ui-ring-neutral-100 ui-bg-white ui-rounded-md
-         ui-p-[5px]  
-        ui-will-change-[opacity,transform] dark:ui-bg-neutral-800
-  `}
+        className={`ui-min-w-[220px] ui-bg-white ui-rounded-md ui-p-[5px] 
+        dark:ui-bg-neutral-800 ui-border ui-border-neutral-100 dark:ui-border-neutral-700
+        `}
       >
         {props.children}
       </RadixDropdown.Content>

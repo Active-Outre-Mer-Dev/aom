a4e956e9d8aec9c59c34d429490a3447d4b9f94d
bluepnwage <a.carty2555@gmail.com>
2023-05-19 18:01:14 -0400

    [sxm-quiz] - update learn index page design

--- apps/sxm-quiz/src/app/learn/random-facts.tsx
+++ apps/sxm-quiz/src/app/learn/random-facts.tsx
@@ -1,17 +1,27 @@
 import { Title } from "ui";
 import { facts } from "@/random-facts";
-import { Button } from "./client";
+import { ChevronLeft, ChevronRight } from "lucide-react";
+
 export function RandomFacts() {
   return (
-    <div className="rounded-xl text-center h-56 flex flex-col justify-between p-4 bg-gradient-to-r from-primary-600 to-primary-300 mx-auto w-3/4 text-white">
-      <Title order={2} className="font-heading mb-2">
+    <div className="rounded-xl relative flex flex-col items-center  radial-gradient text-center h-56  p-4  mx-auto w-3/4 text-white">
+      <Title order={2} className="font-heading ">
         Did you know?
       </Title>
-      <p className="text-primary-50 text-xl mb-4">{facts[0]}</p>
-      <div className="flex justify-center gap-2">
-        <Button variant={"neutral"}>Previous</Button>
-        <Button variant={"neutral"}>Next</Button>
-      </div>
+      <p className="text-primary-50 text-xl mt-8">{facts[0]}</p>
+
+      <button
+        className={`absolute flex items-center justify-center bg-white h-7 w-7 rounded-full
+       text-primary-600 top-2/4 -translate-y-2/4 left-4`}
+      >
+        <ChevronLeft size={"75%"} />
+      </button>
+      <button
+        className={`absolute flex items-center justify-center bg-white h-7 w-7 rounded-full
+       text-primary-600 top-2/4 -translate-y-2/4 right-4`}
+      >
+        <ChevronRight size={"75%"} />
+      </button>
     </div>
   );
 }

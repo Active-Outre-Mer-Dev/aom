4f143c946d730f622eb569aa613e8c55af883e90
bluepnwage <a.carty2555@gmail.com>
2023-05-24 14:25:14 -0400

    [sxm-quiz] - update navbar and footer

--- apps/sxm-quiz/src/components/Nav.tsx
+++ apps/sxm-quiz/src/components/Nav.tsx
@@ -1,18 +1,23 @@
 import { NavItem } from "./NavItem";
-import { Button } from ".";
+import { Button, TextInput } from ".";
+import { Search } from "lucide-react";
 
 export function Nav() {
   return (
-    <header className="h-16 border-b z-50 bg-white sticky top-0 left-0 items-center border-neutral-100">
+    <header className="border-b z-50 bg-white sticky top-0 left-0 items-center border-neutral-100">
       <div className="container mx-auto h-full flex items-center justify-between">
-        <nav>
-          <ul className="flex gap-4">
-            <NavItem href={"/"}>Home</NavItem>
-            <NavItem href={"/quiz"}>Quiz</NavItem>
-            <NavItem href={"/learn"}>Learn</NavItem>
-          </ul>
-        </nav>
-        <div>
+        <div className="flex gap-10 items-center">
+          <span className="font-heading leading-none text-2xl font-medium">SXM Quiz</span>
+          <nav className="h-full">
+            <ul className="flex h-full gap-4">
+              <NavItem href={"/"}>Home</NavItem>
+              <NavItem href={"/quiz"}>Quiz</NavItem>
+              <NavItem href={"/learn"}>Learn</NavItem>
+            </ul>
+          </nav>
+        </div>
+        <div className="flex items-center gap-4 ">
+          <TextInput icon={<Search size={14} className="text-neutral-500" />} placeholder="Search..." />
           <Button variant="neutral">Sign in</Button>
         </div>
       </div>
--- apps/sxm-quiz/src/components/NavItem.tsx
+++ apps/sxm-quiz/src/components/NavItem.tsx
@@ -2,12 +2,20 @@
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import type { LinkProps } from "next/link";
+import { motion } from "framer-motion";
 
 export function NavItem(props: LinkProps & { children: React.ReactNode }) {
   const path = usePathname();
   return (
-    <li className={`text-lg font-medium ${path === props.href ? "text-primary-600" : "text-neutral-600"}`}>
+    <li className={`text-lg flex items-center relative font-medium py-4`}>
       <Link {...props}>{props.children}</Link>
+      {path === props.href && (
+        <motion.span
+          layout
+          layoutId="activeBorder"
+          className="bg-primary-500 absolute rounded-t-lg block h-[3px] w-full bottom-0 left-0"
+        />
+      )}
     </li>
   );
 }

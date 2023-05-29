94b8c9a27512cc603061796ff4f60e24fd390608
bluepnwage <a.carty2555@gmail.com>
2023-05-29 08:36:18 -0400

    [sxm-quiz] - fix dark mode

--- apps/sxm-quiz/tailwind.config.js
+++ apps/sxm-quiz/tailwind.config.js
@@ -3,6 +3,7 @@ const sharedConfig = require("tailwind-config/tailwind.config.js");
 module.exports = {
   presets: [sharedConfig],
   content: [`src/**/*.{js,ts,jsx,tsx,mdx}`, "./node_modules/@aom/ui/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "class",
   theme: {
     extend: {
       fontFamily: {

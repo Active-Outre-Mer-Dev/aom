f2617096d61daebe6f5ef83eaa3af04b9a7bac05
bluepnwage <a.carty2555@gmail.com>
2023-06-13 14:00:40 -0400

    [sxm-quiz] - fallback for quiz summary

--- apps/sxm-quiz/src/app/quiz/[slug]/_components/quiz-tabs/summary.tsx
+++ apps/sxm-quiz/src/app/quiz/[slug]/_components/quiz-tabs/summary.tsx
@@ -1,6 +1,7 @@
 import { Button } from "@aom/ui";
 import { useQuiz } from "../container/container.context";
 import { Suspense, lazy } from "react";
+import { SummaryLoad } from "./summary-load";
 
 const ListSummary = lazy(() => import("../list-quiz/list-summary"));
 const QuestionSummary = lazy(() => import("../question-quiz/summary"));
@@ -9,7 +10,7 @@ export function SummaryResult() {
   const { onReset, type } = useQuiz();
   return (
     <div className=" basis-2/5 flex flex-col  gap-4 justify-between pr-4 py-6 -my-4">
-      <Suspense fallback={<p>Loading summary...</p>}>
+      <Suspense fallback={<SummaryLoad />}>
         {type === "question" ? <QuestionSummary /> : <ListSummary />}
       </Suspense>
       <Button fullWidth onClick={onReset} className="block mt-5">

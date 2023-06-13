413f0023abc4baa0ad2190c311137358bfe37620
bluepnwage <a.carty2555@gmail.com>
2023-06-13 19:34:49 -0400

    [ui] Radio - refactor radio component and fix styles

--- packages/ui/src/radio/index.tsx
+++ packages/ui/src/radio/index.tsx
@@ -2,23 +2,24 @@ import * as RadixRadio from "@radix-ui/react-radio-group";
 import { useId } from "react";
 import { Label } from "../label";
 
-export type RadioProps = { label?: string } & RadixRadio.RadioGroupItemProps;
+export type RadioItem = { label?: string } & RadixRadio.RadioGroupItemProps;
+export type RadioProps = RadixRadio.RadioGroupProps;
 
-export function Radio({ label, ...props }: RadioProps) {
+function Item({ label, ...props }: RadioItem) {
   const id = useId();
   return (
     <div className="flex items-center gap-2">
       <RadixRadio.Item
         {...props}
         id={props.id || id}
-        className={`data-[state=checked]:primary-600 data-[state=checked]:dark:primary-500 
-        neutral-200 dark:neutral-700 overflow-hidden w-5 h-5 rounded-full outline-none 
+        className={`data-[state=checked]:bg-primary-600 data-[state=checked]:dark:bg-primary-500 
+        bg-neutral-200 dark:bg-neutral-700 overflow-hidden w-5 h-5 rounded-full outline-none 
         cursor-default duration-200 ease-out`}
       >
         <RadixRadio.Indicator
-          className={`flex overflow-hidden primary-600 dark:primary-500 items-center 
+          className={`flex overflow-hidden bg-primary-600 dark:bg-primary-500 items-center 
           justify-center w-full h-full relative after:content-[''] after:block 
-          after:w-2/4 after:h-2/4 after:rounded-full after:primary-50`}
+          after:w-2/4 after:h-2/4 after:rounded-full after:bg-primary-50`}
         />
       </RadixRadio.Item>
       {label ? <Label htmlFor={props.id || id}>{label}</Label> : null}
@@ -26,7 +27,7 @@ export function Radio({ label, ...props }: RadioProps) {
   );
 }
 
-function Group(props: RadixRadio.RadioGroupProps) {
+export function Radio(props: RadioProps) {
   return (
     <RadixRadio.Root {...props} className="flex flex-col gap-2.5">
       {props.children}
@@ -34,4 +35,4 @@ function Group(props: RadixRadio.RadioGroupProps) {
   );
 }
 
-Radio.Group = Group;
+Radio.Item = Item;

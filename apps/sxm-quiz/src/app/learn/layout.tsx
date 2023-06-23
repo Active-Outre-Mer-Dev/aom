8e890598ee988a80259cf172aaaeb96d8ff601ca
bluepnwage <a.carty2555@gmail.com>
2023-06-23 14:32:56 -0400

    [sxm-quiz] - small change

--- apps/sxm-quiz/src/app/learn/layout.tsx
+++ apps/sxm-quiz/src/app/learn/layout.tsx
@@ -1,20 +1,10 @@
 import { NavLink } from "./nav-link";
-import { BookOpen, LayoutGrid } from "lucide-react";
+import { LayoutGrid } from "lucide-react";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <main>
       <nav style={{ height: "calc(100vh - 64px)" }} className="w-1/6 p-4  space-y-6 fixed top-16 left-0">
-        <div>
-          <p className="text-lg text-neutral-700 font-medium mb-2 flex items-center gap-2">
-            <BookOpen size={18} className="text-neutral-700 text-lg" />
-            Articles
-          </p>
-          <ul className="w-full border-l-2 border-neutral-100 space-y-[2px]">
-            <NavLink href={"/learn"}>Community articles</NavLink>
-            <NavLink href={"/learn/beta"}>Beta articles</NavLink>
-          </ul>
-        </div>
         <div>
           <p className="text-lg flex items-center gap-2 text-neutral-700 font-medium mb-2">
             <LayoutGrid size={18} className="text-neutral-700" />

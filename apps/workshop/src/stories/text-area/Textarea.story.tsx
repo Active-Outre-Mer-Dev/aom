c2a2ef8c9c656d4887fd7d845eb676caecf1a4f4
Agis Carty <a.carty2555@gmail.com>
2023-08-03 20:05:29 -0400

    [ui] Textarea - sync styling with other inputs

--- apps/workshop/src/stories/text-area/Textarea.story.tsx
+++ apps/workshop/src/stories/text-area/Textarea.story.tsx
@@ -4,7 +4,9 @@ import { Textarea } from "@aomdev/ui";
 const meta = {
   title: "Molecules/Textarea",
   component: Textarea,
-  args: { placeholder: "Write message", wrapperClass: "w-full" }
+  args: { placeholder: "Write message" },
+  //@ts-expect-error
+  argTypes: { size: { type: "select", options: ["md", "sm", "lg"] } }
 } satisfies Meta<typeof Textarea>;
 export default meta;
 
---
+++ packages/ui/src/textarea/styles.tsx
@@ -0,0 +1,16 @@
+import { cva } from "cva";
+
+export const textareaStyles = cva(
+  `rounded-md   text-neutral-900  border resize-none bg-none relative
+  appearance-none  w-full bg-transparent  
+  placeholder:top-2/4 placeholder:
+placeholder:dark:text-gray-600 placeholder:text-gray-400 pl-2
+border-neutral-300 outline-none 
+ data-[error=true]:border-error-600 
+data-[error=true]:dark:border-error-400 
+dark:border-neutral-700  dark:text-neutral-200  
+focus-within:ring focus-within:ring-offset-2 ring-0 
+ring-offset-neutral-50 dark:ring-offset-neutral-900 dark:ring-primary-500 ring-primary-600
+data-[error=true]:ring-error-400 data-[error=true]:dark:ring-error-600
+`
+);

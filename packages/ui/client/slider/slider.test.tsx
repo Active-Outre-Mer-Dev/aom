13d8c4aba398bb4b463b05f8830b77c6c8d8986c
Agis Carty <a.carty2555@gmail.com>
2024-04-23 07:20:32 -0400

    [ui] - remove slider test

--- packages/ui/client/slider/slider.test.tsx
+++
@@ -1,9 +0,0 @@
-import { expect, test } from "@playwright/experimental-ct-react";
-import { Slider } from ".";
-
-test.describe("ui/slider", () => {
-  test("is visible", async ({ mount }) => {
-    const component = await mount(<Slider />);
-    await expect(component.getByRole("slider")).toBeVisible();
-  });
-});
